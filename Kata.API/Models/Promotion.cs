a5dcc89d9787c80ae928329e03e8587f848a490e
devrajtokaram <tokaramd90@gmail.com>
2022-07-10 18:40:05 +0100

    Added models

---
+++ Kata.API/Models/Promotion.cs
@@ -0,0 +1,24 @@
+﻿using Kata.API.Models.Enums;
+
+namespace Kata.API.Models;
+
+/// <summary>
+/// Product promotion
+/// </summary>
+public class Promotion
+{
+    /// <summary>
+    /// Number of items that needs to buy to be eligible for the offer
+    /// </summary>
+    public int NumberOfItemsToBuyForEligibility { get; set; } //_ApplypromotionFor 
+
+    /// <summary>
+    /// type of promotion
+    /// </summary>
+    public PromotionType PromotionType { get; set; } = PromotionType.None;
+
+    /// <summary>
+    /// value of the promotion - percentage/price/something else
+    /// </summary>
+    public decimal Value { get; set; }
+}
