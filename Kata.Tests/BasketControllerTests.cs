9f242e45d2eda18dbd21d6a060c469d10ce9fff6
devrajtokaram <tokaramd90@gmail.com>
2022-07-10 20:02:15 +0100

    Added unit tests for adding item to the basket

--- Kata.Tests/BasketControllerTests.cs
+++ Kata.Tests/BasketControllerTests.cs
@@ -1,16 +1,119 @@
-namespace Kata.Tests
-{
-    public class BasketControllerTests
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
+using System.Net;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Kata.API.Controllers;
+using Kata.API.Services;
+using Kata.API.Models;
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
+namespace Kata.Tests;
+
+public class BasketControllerTests
+{
+    private Mock<ILogger<BasketController>> _logger = default!;
+    private Mock<IBasketService> _mockBasketService = default!;
+    private BasketController _basketController = default!;
+
+    [SetUp]
+    public void Setup()
+    {
+        _logger = new Mock<ILogger<BasketController>>();
+        _mockBasketService = new Mock<IBasketService>();
+        _basketController = new BasketController(
+            _logger.Object,
+            _mockBasketService.Object
+        );
     }
+
+    #region AddItemToBasket tests   
+    [Test]
+    public void AddItemToBasket_ValidItem_ShouldReturnOk()
+    {
+        //Arrange
+        var item = "A";
+        var quantity = 2;
+
+        //Act
+        var response = _basketController.AddItemToBasket(item, quantity);
+        var result = response as OkResult;
+
+        //Assert
+        Assert.IsNotNull(response);
+        Assert.IsNotNull(result);
+        Assert.AreEqual(result.StatusCode, (int)HttpStatusCode.OK);
+    }
+
+    [Test]
+    public void AddItemToBasket_InValidSku_ShouldReturnBadRequest()
+    {
+        //Arrange
+        var item = string.Empty;
+        var quantity = 2;
+
+        //Act
+        var response = _basketController.AddItemToBasket(item, quantity);
+        var result = response as BadRequestObjectResult;
+
+        //Assert
+        Assert.IsNotNull(response);
+        Assert.IsNotNull(result);
+        Assert.AreEqual(result.StatusCode, (int)HttpStatusCode.BadRequest);
+    }
+
+    [Test]
+    public void AddItemToBasket_InValidCount_ShouldReturnBadRequest()
+    {
+        //Arrange
+        var item = "A";
+        var quantity = 0;
+
+        //Act
+        var response = _basketController.AddItemToBasket(item, quantity);
+        var result = response as BadRequestObjectResult;
+
+        //Assert
+        Assert.IsNotNull(response);
+        Assert.IsNotNull(result);
+        Assert.AreEqual(result.StatusCode, (int)HttpStatusCode.BadRequest);
+    }
+
+    [Test]
+    public void AddItemToBasket_InValidItem_ShouldReturnNotFound()
+    {
+        //Arrange
+        var item = "Invalid SKU";
+        var quantity = 1;
+
+        //Act
+        var response = _basketController.AddItemToBasket(item, quantity);
+        var result = response as NotFoundObjectResult;
+
+        //Assert
+        Assert.IsNotNull(response);
+        Assert.IsNotNull(result);
+        Assert.AreEqual(result.StatusCode, (int)HttpStatusCode.NotFound);
+    }
+
+    [Test]
+    public void AddItemToBasket_CatchException_ShouldReturnBadRequest()
+    {
+        //Arrange
+        var item = "A";
+        var quantity = 1;
+        _mockBasketService.Setup(
+            service =>
+                service.AddItem(It.IsAny<Product>(), It.IsAny<int>())
+                ).Throws<Exception>();
+
+        //Act
+        var response = _basketController.AddItemToBasket(item, quantity);
+        var result = response as BadRequestObjectResult;
+
+        //Assert
+        Assert.IsNotNull(response);
+        Assert.IsNotNull(result);
+        Assert.AreEqual(result.StatusCode, (int)HttpStatusCode.BadRequest);
+    }
+    #endregion
+
 }
