eede7c5ee17c489e2c171c209ed80b10de041346
devrajtokaram <tokaramd90@gmail.com>
2022-07-10 19:41:33 +0100

    Added unit test project

---
+++ Kata.Tests/BasketControllerTests.cs
@@ -0,0 +1,16 @@
+namespace Kata.Tests
+{
+    public class BasketControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
